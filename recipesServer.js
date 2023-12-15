0a53c744bae020a4737ad5656d9010de9a57f2b6
pkandyal <pkandyal@gmail.com>
2023-12-14 20:58:31 -0500

    fixed public

--- recipesServer.js
+++ recipesServer.js
@@ -32,6 +32,43 @@ app.get("/searchPublicRecipe", (request, response) => {
     response.render("searchPublicRecipe");
 });
 
+app.post("/showPublicRecipe", async (request, response) => {
+    const {name} = request.body;
+    let person = await getRecipe(name);
+    response.render("showPublicRecipe", person);
+});
+
+async function getRecipe(name) {
+  const result = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
+  const data = await result.json();
+  let ingredients = "";
+  if (data.meals == null) {
+    let noneFound = {
+        mealName: `"${name}" not found`,
+        ingredientList: "Ingredients Not Found",
+        instructions: "Instructions Not Found",
+      };
+    return noneFound;
+  }
+  for(let i  = 0; i < 20; i++){
+    let ingr = "strIngredient" + (i + 1);
+    let meas = "strMeasure" + (i + 1);
+    if ((data.meals[0][ingr] != "" && data.meals[0][ingr] != null) && (data.meals[0][meas] != " " && data.meals[0][meas] != null)) {
+        ingredients = ingredients + data.meals[0][ingr] + " - " + data.meals[0][meas] + "; ";
+    }
+  }
+  let person = {
+    mealName: data.meals[0].strMeal,
+    ingredientList: ingredients,
+    instructions: data.meals[0].strInstructions,
+  };
+  //console.log(person.mealName + "1");
+  //console.log(person.ingredientList + "1");
+  //console.log(person.instructions);
+  return person;
+
+}
+
 app.listen(5000);
 
 process.stdin.setEncoding("utf8");
